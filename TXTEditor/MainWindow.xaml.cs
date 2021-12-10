2867b15763ed293b42e5efed585cd6774753564a
Roman Permiakov <info@presstomsk.ru>
2021-12-10 19:09:28 +0700

    Выделение методов в отдельный класс

--- TXTEditor/MainWindow.xaml.cs
+++ TXTEditor/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
+﻿
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace TXTEditor
 {
@@ -28,37 +16,22 @@ namespace TXTEditor
         }
         private void Open_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Text Files (*.txt)|*.txt";
-            if (openFileDialog.ShowDialog() == true)
-            {
-                TextB.Text = File.ReadAllText(openFileDialog.FileName);
-                path = openFileDialog.FileName;
-            }
+            TextB.Text = FileDialogues.OpenFile(out string strPath);
+            path = strPath;
         }
 
         private void SaveAs_Click(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                File.WriteAllText(saveFileDialog.FileName, TextB.Text);
-                path = saveFileDialog.FileName;
-            }
+            FileDialogues.SaveAsFile(TextB.Text, out string strPath);
+            path = strPath;
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
             if (path == null)
             {
-                SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
-                if (saveFileDialog.ShowDialog() == true)
-                {
-                    File.WriteAllText(saveFileDialog.FileName, TextB.Text);
-                    path = saveFileDialog.FileName;
-                }
+                FileDialogues.SaveAsFile(TextB.Text, out string strPath);
+                path = strPath;
                 return;
             }
             File.WriteAllText(path, TextB.Text);
